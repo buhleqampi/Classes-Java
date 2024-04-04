dea32d408443e069e642b3e3de321b8cd0754d0a
buhleqampi <buhle3857@gmail.com>
2024-04-04 13:49:09 +0200

    javainfo

--- Main.java
+++ Main.java
@@ -1,8 +1,33 @@
-public class Main {
-    public void log (int [] numbers){
-        for (int number: numbers)
-        // System.out.printIn(number)
-        // System.out.printIn()
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        int[] mynum ={3, 5, 5, 5,6,0,1,9,282,9};
+    
+        Scanner sc = new Scanner(System.in);
+        System.out.print(">>>>>> ");
+        String name = sc.nextLine();
+
+        sc.close();
+
+
+        System.out.println("HEYY " + name);
+
+
+        int[] myArr = new int[10];
+        
+
+        log(mynum);
+        
+    }
+
+    private static void log(int[] numbers)
+    {
+        for(int sfiso: numbers){
+            System.out.println(sfiso);
+        }
     }
 }
-    public void log (                          )
